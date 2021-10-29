4d0de886011c88f9907f6f6476016b25a17c547c
yhtiyar <yhtiyarsahatow@gmail.com>
2021-10-29 21:58:30 +0300

    erc20

--- centipede/manage.py
+++ centipede/manage.py
@@ -8,22 +8,9 @@ from web3 import Web3
 from web3.contract import Contract
 from web3.types import ABIEvent, ABIFunction
 
-from centipede.contracts import ERC1155, CentipedeContract
+from centipede.contracts import ERC1155, ERC20, ERC721, CentipedeContract
 
 from .web3_util import deploy_contract
-
-
-def init_web3(ipc_path: str) -> Web3:
-    return Web3(web3.HTTPProvider(ipc_path))
-
-
-def init_contract(
-    web3: Web3, abi: Dict[str, Any], address: Optional[str]
-) -> Union[Contract, Type[Contract]]:
-    checksum_address: Optional[ChecksumAddress] = None
-    if address is not None:
-        checksum_address = web3.toChecksumAddress(address)
-    return web3.eth.contract(address=checksum_address, abi=abi)
 
 
 def _deploy_centipede_token_contract(
@@ -60,6 +47,48 @@ def deploy_ERC1155(
     return _deploy_centipede_token_contract(
         web3,
         ERC1155,
+        token_name,
+        token_symbol,
+        token_uri,
+        token_owner,
+        deployer,
+        deployer_private_key,
+    )
+
+
+def deploy_ERC20(
+    web3: Web3,
+    token_name: str,
+    token_symbol: str,
+    token_uri: str,
+    token_owner: ChecksumAddress,
+    deployer: ChecksumAddress,
+    deployer_private_key: str,
+) -> Tuple[HexBytes, ChecksumAddress]:
+    return _deploy_centipede_token_contract(
+        web3,
+        ERC20,
+        token_name,
+        token_symbol,
+        token_uri,
+        token_owner,
+        deployer,
+        deployer_private_key,
+    )
+
+
+def deploy_ERC721(
+    web3: Web3,
+    token_name: str,
+    token_symbol: str,
+    token_uri: str,
+    token_owner: ChecksumAddress,
+    deployer: ChecksumAddress,
+    deployer_private_key: str,
+) -> Tuple[HexBytes, ChecksumAddress]:
+    return _deploy_centipede_token_contract(
+        web3,
+        ERC721,
         token_name,
         token_symbol,
         token_uri,
