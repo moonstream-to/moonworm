5679e4fd627db91f9761e8fc24ee27496242e93c
yhtiyar <yhtiyarsahatow@gmail.com>
2021-11-03 15:53:33 +0300

    fix cli

--- moonworm/cli.py
+++ moonworm/cli.py
@@ -32,6 +32,10 @@ def create_init_py(dest_dir: str, force: bool = False) -> None:
 
 
 def handle_generate(args: argparse.Namespace) -> None:
+    if not args.interface and not args.cli:
+        print("Please specify what you want to generate:")
+        print("--interface for smart contract interface")
+        print("--cli for smart contract cli")
     args.name = args.name + "_"
 
     if args.abi == "erc20":
@@ -68,7 +72,7 @@ def generate_argument_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description="Moonworm: Manage your smart contract")
 
     parser.set_defaults(func=lambda _: parser.print_help())
-    subcommands = parser.add_subparsers()
+    subcommands = parser.add_subparsers(dest="subcommands")
 
     generate_parser = subcommands.add_parser(
         "generate", description="Moonworm code generator"
