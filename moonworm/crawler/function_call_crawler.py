87ea6a891b1db152e6dac442481f76e43f8b39af
Neeraj Kashyap <nkashy1@gmail.com>
2021-11-07 07:50:15 -0800

    Added PickleFileState implementation for FunctionCallCrawlerState
    Modified `FunctionCallCrawlerState` interface a bit.
    
    Also fixed `FunctionCallCrawler` implementation and added tests against
    the `web3.EthereumTesterProvider`.

--- moonworm/crawler/function_call_crawler.py
+++ moonworm/crawler/function_call_crawler.py
@@ -1,9 +1,11 @@
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass
+import os
+import pickle
 from typing import Any, Dict, List
+
 from eth_typing.evm import ChecksumAddress
 from web3 import Web3
-import web3
 from web3.contract import Contract
 from web3.types import ABI
 
@@ -56,21 +58,73 @@ class FunctionCallCrawlerState(ABC):
     """
 
     @abstractmethod
-    def get_last_crawled_block() -> int:
+    def get_last_crawled_block(self) -> int:
         """
         Returns the last block number that was crawled.
         """
         pass
 
     @abstractmethod
-    def process_function_call(self, function_call: ContractFunctionCall):
+    def register_call(self, function_call: ContractFunctionCall) -> None:
         """
         Processes the given function call (store it, etc.).
         """
         pass
 
+    @abstractmethod
+    def flush(self) -> None:
+        """
+        Flushes cached state to storage layer.
+        """
+        pass
 
-class EthereumStateProviderW3Impl(EthereumStateProvider):
+
+class PickleFileState(FunctionCallCrawlerState):
+    """
+    Implements the FunctionCallCrawlerState interface using a JSON file in which to store the safe.
+
+    Note: Does not implement any file lock on the JSON file, and assumes that no other process is
+    modifying the file at the same time. Does not perform atomic write to the file, either, so
+    readers beware.
+    """
+
+    def __init__(self, pickle_file: str, batch_size: int = 100):
+        initial_state = {
+            "last_crawled_block": -1,
+            "calls": [],
+        }
+
+        self.state = initial_state
+
+        if not os.path.exists(pickle_file):
+            with open(pickle_file, "wb") as ofp:
+                pickle.dump(initial_state, ofp)
+        else:
+            with open(pickle_file, "rb") as ifp:
+                self.state = pickle.load(ifp)
+        self.pickle_file = pickle_file
+
+        self.state_file = pickle_file
+        self.batch_size = batch_size
+        self.cache_size = 0
+
+    def get_last_crawled_block(self) -> int:
+        return self.state.get("last_crawled_block")
+
+    def register_call(self, function_call: ContractFunctionCall) -> None:
+        self.state["calls"].append(asdict(function_call))
+        self.cache_size += 1
+        self.state["last_crawled_block"] = function_call.block_number
+        if self.cache_size == self.batch_size:
+            self.flush()
+
+    def flush(self) -> None:
+        with open(self.pickle_file, "wb") as ofp:
+            pickle.dump(self.state, ofp)
+        self.cache_size = 0
+
+
+class Web3StateProvider(EthereumStateProvider):
     """
     Implementation of EthereumStateProvider with web3.
     """
@@ -111,14 +165,13 @@ class FunctionCallCrawler:
         self.contract = Web3().eth.contract(abi=self.contract_abi)
 
     def process_transaction(self, transaction: Dict[str, Any]):
-
-        raw_function_call = self.contract.decode_function_input(transaction["input"])
+        raw_function_call = self.contract.decode_function_input(transaction["data"])
         function_name = raw_function_call[0].fn_name
         function_args = raw_function_call[1]
         function_call = ContractFunctionCall(
-            block_number=transaction["blockNumber"],
+            block_number=transaction["block_number"],
             block_timestamp=self.ethereum_state_provider.get_block_timestamp(
-                transaction["blockNumber"]
+                transaction["block_number"]
             ),
             transaction_hash=transaction["hash"],
             contract_address=transaction["to"],
@@ -126,47 +179,32 @@ class FunctionCallCrawler:
             function_name=function_name,
             function_args=function_args,
         )
-        print("here")
-        self.state.process_function_call(function_call)
+        self.state.register_call(function_call)
 
-    def crawl(self, from_block: int, to_block: int):
+    def crawl(self, from_block: int, to_block: int, flush_state: bool = False):
         for block_number in range(from_block, to_block + 1):
-            print(f"Crawling block {block_number}")
             for address in self.contract_addresses:
                 transactions = self.ethereum_state_provider.get_transactions_to_address(
                     address, block_number
                 )
                 for transaction in transactions:
                     self.process_transaction(transaction)
+            self.state.state
+        if flush_state:
+            self.state.flush()
 
 
-class MockupState(FunctionCallCrawlerState):
-    """
-    Mockup state for function call crawler.
-    """
+# crawler = FunctionCallCrawler(
+#     state=MockupState(),
+#     ethereum_state_provider=Web3StateProvider(
+#         w3=Web3(Web3.HTTPProvider("https://mainnet.infura.io/v3/<Project Id>"))
+#     ),
+#     contract_abi=ERC1155.abi(),
+#     contract_addresses=[
+#         Web3.toChecksumAddress("0x495f947276749ce646f68ac8c248420045cb7b5e"),
+#         Web3.toChecksumAddress("0x3b7335f3f1771122cd0107416b1da1b2fb7e94dd"),
+#     ],
+# )
 
-    def __init__(self):
-        self.last_crawled_block = 0
-
-    def get_last_crawled_block(self):
-        return self.last_crawled_block
-
-    def process_function_call(self, function_call: ContractFunctionCall):
-        print(f"Got a function call: {function_call}")
-        self.last_crawled_block = function_call.block_number
-
-
-crawler = FunctionCallCrawler(
-    state=MockupState(),
-    ethereum_state_provider=EthereumStateProviderW3Impl(
-        w3=Web3(Web3.HTTPProvider("https://mainnet.infura.io/v3/<Project Id>"))
-    ),
-    contract_abi=ERC1155.abi(),
-    contract_addresses=[
-        Web3.toChecksumAddress("0x495f947276749ce646f68ac8c248420045cb7b5e"),
-        Web3.toChecksumAddress("0x3b7335f3f1771122cd0107416b1da1b2fb7e94dd"),
-    ],
-)
-
-current_block_number = crawler.ethereum_state_provider.get_last_block_number()
-crawler.crawl(from_block=current_block_number - 30, to_block=current_block_number)
+# current_block_number = crawler.ethereum_state_provider.get_last_block_number()
+# crawler.crawl(from_block=current_block_number - 30, to_block=current_block_number)
